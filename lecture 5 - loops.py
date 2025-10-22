57f8bb000644a769877eaf667670b6e074d31673
mdayan-dot <md.ayan.sys@gmail.com>
2025-10-22 14:39:09 +0530

    Add files via upload

---
+++ lecture 5 - loops.py
@@ -0,0 +1,32 @@
+#wap to search a no in this tuple using loop
+
+
+list=(1, 3, 4 , 5, 7 , 5 ,8 , 9)
+x=7
+i=0
+while i<len(list):
+    if(list[i]==x):
+     print("found at index",i)
+    i+=1
+
+
+
+#print 1 to 100 using while loop
+
+    i=1
+    while i<=100:
+        print(i)
+        i+=1
+
+
+#print 1 to 38 using for loop
+
+
+        for i in range(1,39):
+         print(i) 
+
+#print a table of a no usnig for loop
+
+         n=int(input("enter any no:"))
+         for i in range(1,11):
+          print(n*i)
